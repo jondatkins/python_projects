b21f59fcfb92ee703da1a9ef9768ef30cc41e9f6
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-08 20:55:14 +0100

    ranges

--- controlflow.py
+++ controlflow.py
@@ -2,3 +2,28 @@
 words = ["cat", "window", "defenestrate"]
 for w in words:
     print(w, len(w))
+# Create a simple collection
+users = {"Hans": "active", "Eleonore": "inactive", "foo": "active"}
+
+print(users)
+# Strategy: Iterate over a copy
+for user, status in users.copy().items():
+    if status == "inactive":
+        del users[user]
+
+print(users)
+# Strategy: Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == "active":
+        active_users[user] = status
+
+print(active_users)
+
+# The range function
+for i in range(5):
+    print(i)
+
+print(list(range(5, 10)))
+print(list(range(0, 10, 3)))
+print(list(range(-10, -100, -30)))
