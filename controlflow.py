6f9784f317ea33a5b8275a0f2e1465fba8ae2784
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-11 18:27:11 +0100

    more on control flow

--- controlflow.py
+++ controlflow.py
@@ -166,3 +166,87 @@ def fib2(n):  # return Fibonacci series up to n
 
 f100 = fib2(100)  # call it
 print(f100)  # write the result
+
+
+# You can have default values for your arguments.
+def ask_ok(prompt, retries=4, reminder="Please try again!"):
+    while True:
+        reply = input(prompt)
+        if reply in {"y", "ye", "yes"}:
+            return True
+        if reply in {"n", "no", "nop", "nope"}:
+            return False
+        retries = retries - 1
+        if retries < 0:
+            print("foo")
+            raise ValueError("invalid user response")
+        print(reminder)
+
+
+# ask_ok("OK to overwrite the file?", 2)
+# ask_ok("Do you really want to quit?")
+
+# call to f here prints 5, not 6
+# i = 5
+# def f(arg=i):
+#     print(arg)
+# i = 6
+# f()
+
+
+# The default 'L' value is evaluated once, so it will accumulate values with
+# repeated calls.
+# def f(a, L=[]):
+#     L.append(a)
+#     return L
+
+
+# Because L points to a list object, we need to give it a 'None' value
+# to prevent it from saving values between function calls
+def f(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+
+print(f(1))
+print(f(2))
+print(f(3))
+
+
+# You can use labels for function arguments, these are called keyword
+# Arguments
+def parrot(voltage, state="a stiff", action="voom", type="Norwegian Blue"):
+    print("-- This parrot wouldn't", action, end=" ")
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+
+parrot(1000)  # 1 positional argument
+parrot(voltage=1000)  # 1 keyword argument
+parrot(voltage=1000000, action="VOOOOOM")  # 2 keyword arguments
+parrot(action="VOOOOOM", voltage=1000000)  # 2 keyword arguments
+parrot("a million", "bereft of life", "jump")  # 3 positional arguments
+parrot("a thousand", state="pushing up the daisies")  # 1 positional, 1 keyword
+
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+
+cheeseshop(
+    "Limburger",
+    "It's very runny, sir.",
+    "It's really very, VERY runny, sir.",
+    shopkeeper="Michael Palin",
+    client="John Cleese",
+    sketch="Cheese Shop Sketch",
+)
