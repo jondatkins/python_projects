5f45710ae410902a502aa539c928168aa0ff32d5
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-12 20:31:32 +0100

    Moving on to automate the boring stuff.

---
+++ images.py
@@ -0,0 +1,10 @@
+from PIL import ImageColor
+
+red = ImageColor.getcolor("red", "RGBA")
+print(red)
+black = ImageColor.getcolor("black", "RGBA")
+print(black)
+choc = ImageColor.getcolor("chocolate", "RGBA")
+print(choc)
+blue = ImageColor.getcolor("CornflowerBlue", "RGBA")
+print(blue)
