b653aa8ad53c97949eed18efd4aa2932bbbc9123
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-18 21:55:42 +0100

    More about time.

---
+++ time/datedemo.py
@@ -0,0 +1,7 @@
+import datetime
+import time
+
+print(datetime.datetime.fromtimestamp(1000000))
+print(datetime.datetime(1970, 1, 12, 5, 46, 40))
+print(datetime.datetime.fromtimestamp(time.time()))
+print(datetime.datetime(2019, 10, 21, 16, 30, 0, 604980))
