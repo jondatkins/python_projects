14e10fbb958e8bf495ea69b23851446555a5637c
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-19 18:33:58 +0100

    More about time.

--- time/pause.py
+++ time/pause.py
@@ -1,6 +1,24 @@
 import datetime
 import time
 
-someDate = datetime.datetime(2024, 8, 19, 10, 59, 0)
+someDate = datetime.datetime(2024, 8, 19, 17, 47, 0)
 while datetime.datetime.now() < someDate:
+    print("sleeping")
     time.sleep(1)
+print("awake")
+
+
+oct21st = datetime.datetime(2019, 10, 21, 16, 29, 0)
+print(oct21st.strftime("%Y/%m/%d %H:%M:%S"))
+print(oct21st.strftime("%I:%M %p"))
+print(oct21st.strftime("%B of '%y"))
+
+
+print(datetime.datetime.strptime("October 21, 2019", "%B %d, %Y"))
+print(datetime.datetime(2019, 10, 21, 0, 0))
+print(datetime.datetime.strptime("2019/10/21 16:29:00", "%Y/%m/%d %H:%M:%S"))
+print(datetime.datetime(2019, 10, 21, 16, 29))
+print(datetime.datetime.strptime("October of '19", "%B of '%y"))
+print(datetime.datetime(2019, 10, 1, 0, 0))
+print(datetime.datetime.strptime("November of '63", "%B of '%y"))
+print(datetime.datetime(2063, 11, 1, 0, 0))
