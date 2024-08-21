44bd8e88f9502704f817bcd184e64fe108df7892
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-21 11:31:21 +0100

    Copying countdown program from book. This has 'flush=True', so the terminal will not buffer single line output.

--- time/countdown.py
+++ time/countdown.py
@@ -1,10 +1,21 @@
 import time
 import subprocess
-import os
 
-for i in range(10):
-    print(i)
-    time.sleep(1)
-subprocess.run(["paplay", "mixkit-classic-alarm-995.wav"])
+# for i in range(10):
+#     print(i)
+#     time.sleep(1)
+# timeLeft = 60
+# while timeLeft > 0:
+#     print(timeLeft, end="")
+#     time.sleep(1)
+#     timeLeft -= 1
 # file = "mixkit-classic-alarm-995.wav"
 # os.system("paplay " + file)
+timeLeft = 10
+while timeLeft > 0:
+    # Add flush=True here so that each number is printed every second,
+    # rather than all at the end of the countdown
+    print(timeLeft, end=" ", flush=True)
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+subprocess.run(["paplay", "mixkit-classic-alarm-995.wav"])
