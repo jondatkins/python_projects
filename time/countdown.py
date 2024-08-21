1952e9b6a1f347ef8bd688e35598fc20b8478216
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-21 11:33:28 +0100

    Tidying up commented out code.

--- time/countdown.py
+++ time/countdown.py
@@ -1,16 +1,10 @@
 import time
 import subprocess
 
+# This works, but is not what's in the book
 # for i in range(10):
 #     print(i)
 #     time.sleep(1)
-# timeLeft = 60
-# while timeLeft > 0:
-#     print(timeLeft, end="")
-#     time.sleep(1)
-#     timeLeft -= 1
-# file = "mixkit-classic-alarm-995.wav"
-# os.system("paplay " + file)
 timeLeft = 10
 while timeLeft > 0:
     # Add flush=True here so that each number is printed every second,
@@ -19,3 +13,6 @@ while timeLeft > 0:
     time.sleep(1)
     timeLeft = timeLeft - 1
 subprocess.run(["paplay", "mixkit-classic-alarm-995.wav"])
+# Another way of playing a sound using pulse audio
+# file = "mixkit-classic-alarm-995.wav"
+# os.system("paplay " + file)
