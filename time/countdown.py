401056c1fd4c75245ccfd452d39d42ccec5b91e0
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-21 10:18:50 +0100

    Countdown program from automate the boring stuff.

--- time/countdown.py
+++ time/countdown.py
@@ -2,6 +2,9 @@ import time
 import subprocess
 import os
 
+for i in range(10):
+    print(i)
+    time.sleep(1)
 subprocess.run(["paplay", "mixkit-classic-alarm-995.wav"])
 # file = "mixkit-classic-alarm-995.wav"
 # os.system("paplay " + file)
--- time/launchWindowsApp.py
+++ time/launchWindowsApp.py
@@ -1,4 +1,5 @@
 from re import sub
 import subprocess
 
+# This works on wsl 2, where the 'C' drive is at /mnt/c
 paintProc = subprocess.Popen("/mnt/c/Program Files/ImageGlass/ImageGlass.exe")
