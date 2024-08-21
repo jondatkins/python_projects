573c13f8f1e089ddf71dedb6cb830864bbb123e0
jondatkins <jon.david.atkins@gmail.com>
2024-08-21 16:00:01 +0100

    Starting at the beginning.

---
+++ automate/flowcontrol/gaus.py
@@ -0,0 +1,10 @@
+# Calculate the sum of all numbers in a given range
+total = 0
+num2 = 101
+half = 101 // 2
+for num in range(101):
+    total = total + num
+print(total)
+# You can also do 50*101 to get this value
+sum = half * num2
+print(sum)
---
+++ automate/flowcontrol/vampire.py
@@ -0,0 +1,12 @@
+name = "Carol"
+age = 30
+if name == "Alice":
+    print("Hi, Alice")
+elif age < 12:
+    print("You are not Alice, Kiddo.")
+elif age > 2000:
+    print("Unlike you, Alice is not an undead, immortal vampire.")
+elif age > 100:
+    print("You are not Alice, grannie.")
+else:
+    print("You are neither Alice, a little kid or a grannie.")
