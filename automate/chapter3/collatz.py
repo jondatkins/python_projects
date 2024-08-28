1105800a9228fe41689ac38c13e1b6735fa1a578
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-08-28 13:48:06 +0100

    A little more python work.

--- automate/chapter3/collatz.py
+++ automate/chapter3/collatz.py
@@ -1,3 +1,7 @@
+import sys
+import time
+
+
 def collatz(number):
     if number % 2 == 0:
         print(number // 2)
@@ -8,7 +12,17 @@ def collatz(number):
 
 
 print("Enter a number:")
-number = int(input())
+number = 1
+try:
+    number = int(input())
+    if number < 0:
+        print("number can't be negative")
+        # sys.exit()
+        raise ValueError("foo")
 
+except ValueError:
+    print("Please enter a positive integer")
+    # sys.exit()
 while number != 1:
     number = collatz(number)
+    time.sleep(1)
