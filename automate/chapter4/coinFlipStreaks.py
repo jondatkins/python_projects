fff8551a47398ff01d949127573f34314d7a9d08
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-09-19 22:05:53 +0100

    Adding some comments, checking logic in debugger.

--- automate/chapter4/coinFlipStreaks.py
+++ automate/chapter4/coinFlipStreaks.py
@@ -2,9 +2,10 @@ import random
 
 numberOfStreaks = 0
 headsTails = ["H", "T"]
-experimentRange = 10000
+# experimentRange = 10000
+experimentRange = 1
 testToss = [
-    "T",
+    "H",
     "H",
     "H",
     "H",
@@ -24,6 +25,9 @@ testToss = [
 # coinTossLength = len(testToss)
 coinTossLength = 100
 streakLength = 6
+# streakLength = 3
+
+# You can use the testToss list to debug, by uncommenting lines with testToss in them. You can also shorten the streak length to make life easier
 
 for experimentNumber in range(experimentRange):
     # Code that creates a list of 100 'heads' or 'tails' values.
@@ -38,9 +42,9 @@ for experimentNumber in range(experimentRange):
         matchCount = 1
         y = i + 1
         end = i + streakLength
+        # loop over each potential streak
         while end < len(headsOrTails) and y < end:
-            # print("ith value is: %s" % headsOrTails[i])
-            # print("yth value is: %s" % headsOrTails[y])
+            # if i and y are different, there's no streak, so break to outer loop and start again
             if headsOrTails[i] != headsOrTails[y]:
                 break
             else:
@@ -48,10 +52,13 @@ for experimentNumber in range(experimentRange):
             y += 1
         if matchCount == streakLength:
             numberOfStreaks += 1
-            i = i + streakLength
+            # This index update makes sense if we are counting all the streaks in a run of coin tosses.
+            # However, we break to the outer for loop here, so there's no reason to do this.
+            # i = i + streakLength
             # We're only looking for one streak in our run of 100 coin tosses, so break to outer loop here
             break
         else:
+            # increment i for inner loop where no match is found
             i = i + 1
 # numberOfStreaks / 10000 * 100 is the same as numberOfStreaks / 100
 print("Chance of streak: %s%%" % (numberOfStreaks / coinTossLength))
