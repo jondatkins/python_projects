1003ad46f7e0ea90bc40125eba98060b939d817f
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-09-23 20:22:03 +0100

    Starting on chapter 4, dictionaries.

---
+++ automate/chapter4/2dArrayHeart.py
@@ -0,0 +1,18 @@
+grid = [
+    [".", ".", ".", ".", ".", "."],
+    [".", "O", "O", ".", ".", "."],
+    ["O", "O", "O", "O", ".", "."],
+    ["O", "O", "O", "O", "O", "."],
+    [".", "O", "O", "O", "O", "O"],
+    ["O", "O", "O", "O", "O", "."],
+    ["O", "O", "O", "O", ".", "."],
+    [".", "O", "O", ".", ".", "."],
+    [".", ".", ".", ".", ".", "."],
+]
+gridHeight = len(grid)
+gridWidth = len(grid[0])
+
+for i in range(0, gridWidth):
+    print()
+    for j in range(0, gridHeight):
+        print(grid[j][i], end="")
