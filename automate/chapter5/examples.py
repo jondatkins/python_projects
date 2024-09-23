55656e502f37ba78f922bff64de16556912fcdbb
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-09-23 21:03:46 +0100

    Finished reading chapter, starting exercises.

--- automate/chapter5/examples.py
+++ automate/chapter5/examples.py
@@ -8,3 +8,17 @@ for character in message:
     count[character] = count[character] + 1
 
 pprint.pprint(count)
+
+
+spam = {"name": "Pooka", "age": 5}
+if "color" not in spam:
+    spam["color"] = "black"
+print(spam["color"])
+
+# spam = {"bar": 100}
+# print(spam["foo"])
+# print(spam.get("foo", 0))
+spam = {"name": "Pooka", "age": 5}
+spam.setdefault("color", "black")
+print(spam["color"])
+print(spam)
