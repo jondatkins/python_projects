4815adc47a1e355d6e54a09454b4af3bf6c0807d
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-10-20 19:28:57 +0100

    Program works after setting display var and re-activating virtual env.

--- automate/chapter6/phoneAndEmail.py
+++ automate/chapter6/phoneAndEmail.py
@@ -21,3 +21,25 @@ emailRegex = re.compile(
    )""",
     re.VERBOSE,
 )
+text = str(pyperclip.paste())
+
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = "-".join([groups[1], groups[3], groups[5]])
+    if groups[8] != "":
+        phoneNum += " x" + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# TODO: Copy results to the clipboard.
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy("\n".join(matches))
+    print("Copied to clipboard:")
+    print("\n".join(matches))
+else:
+    print("No phone numbers or email addresses found.")
