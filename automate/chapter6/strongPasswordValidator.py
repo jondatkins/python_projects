ca80def32eab3fa56632d4623efd2d7a35086986
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-10-23 14:39:06 +0100

    More validation, with Google help.

--- automate/chapter6/strongPasswordValidator.py
+++ automate/chapter6/strongPasswordValidator.py
@@ -7,10 +7,15 @@ uppercaseRegEx = re.compile(r"([A-Z])")
 lowercaseRegEx = re.compile(r"([a-z])")
 numbersRegEx = re.compile(r"([0-9])")
 upperAndLowerRegEx = re.compile(r"(?=.*[a-z])(?=.*[A-Z]).*")
-
+# This is the first google answer, this doesn't check for upper and lower case.
+googlesRegEx = re.compile(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+# This is the answer from google, matching upper and lower case
+googlesRegEx2 = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
 
 # (.{8,})
-badPasword = "123"
+badPassword = "123"
+badPassword2 = "iamtheverymodel"
+badPassword3 = "iamtheverymodel4"
 goodPassword = "iAmTheVeryModelOfAModernMajorGeneral2"
 allCaps = "I AM SHOUTING!!!!"
 lwrCase = "hello? is it me you're looking for?"
@@ -26,9 +31,11 @@ def checkPassword(regEx, password):
         print("{0} is a strong password".format(password))
 
 
-checkPassword(lengthRegEx, badPasword)
-checkPassword(lengthRegEx, goodPassword)
-checkPassword(uppercaseRegEx, allCaps)
-checkPassword(lowercaseRegEx, lwrCase)
-checkPassword(numbersRegEx, numbers)
-checkPassword(upperAndLowerRegEx, lowerAndUpper)
+# checkPassword(lengthRegEx, badPasword)
+# checkPassword(lengthRegEx, goodPassword)
+# checkPassword(uppercaseRegEx, allCaps)
+# checkPassword(lowercaseRegEx, lwrCase)
+# checkPassword(numbersRegEx, numbers)
+# checkPassword(upperAndLowerRegEx, lowerAndUpper)
+
+checkPassword(googlesRegEx, badPassword2)
