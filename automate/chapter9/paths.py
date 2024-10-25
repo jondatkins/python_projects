8854509bf6dd94fe6729ae08e2fd4b4e7c490f6f
jondatkins <jon.david.atkins@gmail.com>
2024-10-25 12:12:42 +0100

    Reading about paths / pathlibe

--- automate/chapter9/paths.py
+++ automate/chapter9/paths.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
 
-print(Path("spam", "bacon", "eggs"))
-print(str(Path("spam", "bacon", "eggs")))
+print(Path.cwd())
+print(Path.home())
+print(os.path.abspath("."))
+print(os.path.isabs("."))
+print(os.path.isabs(os.path.abspath(".")))
